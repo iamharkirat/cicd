6a93acb034bbb090908e8fc33bf5dec548359795
iamharkirat <hchahal1@asu.edu>
2023-09-21 16:01:00 -0700

    added hyperparameter tuning

--- src/utils.py
+++ src/utils.py
@@ -1,10 +1,13 @@
 import os
 import sys
-import numpy as np
+
+import numpy as np 
 import pandas as pd
 import dill
 import pickle
+from src.logger import logging
 from sklearn.metrics import r2_score
+from sklearn.model_selection import GridSearchCV
 
 from src.exception import CustomException
 
@@ -19,13 +22,21 @@ def save_object(file_path, obj):
     except Exception as e:
         raise CustomException(e, sys)
 
-def evaluate_models(X_train, y_train, X_test, y_test, models):
+def evaluate_models(X_train, y_train, X_test, y_test, models, param):
     try:
         report={}
     
         for i in range(len(list(models))):
             model = list(models.values())[i]
-            model.fit(X_train, y_train)
+            para=param[list(models.keys())[i]]
+
+            logging.info('Start hyperparameter tuning')
+            gs = GridSearchCV(model,para,cv=3)
+            gs.fit(X_train,y_train)
+
+            logging.info('Using best hyperparameters to fit the model')
+            model.set_params(**gs.best_params_)
+            model.fit(X_train,y_train)
             
             y_train_pred = model.predict(X_train)
             y_test_pred = model.predict(X_test)
